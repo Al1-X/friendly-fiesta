286f87bc614ead80486e410d7462bab30bf13ec3
AlinaGeo <79804243+AlinaGeo@users.noreply.github.com>
2023-01-22 21:54:12 +0200

    1:14:10

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import CustomForm from "./Components/CustomForm";
+import EditForm from "./Components/EditForm";
 import TaskList from "./Components/TaskList";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
@@ -12,6 +13,8 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-d
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [editedTask, setEditedTask] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   const addTask = (task) => {
     setTasks(prevState => [...prevState, task])
@@ -27,6 +30,25 @@ function App() {
         ? { ...t, checked: !t.checked }
         : t
     )))
+  }
+
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id === task.id
+        ? { ...t, name: task.name }
+        : t
+    )))
+    closeEditMode();
+  }
+
+  const closeEditMode = () => {
+    setIsEditing(false);
+    // previous state focus
+  }
+  
+  const enterEditMode = (task) => {
+    setEditedTask(task);
+    setIsEditing(true);
   }
 
   return (
@@ -49,12 +71,21 @@ function App() {
             <header>
               <h1>My Task List</h1>
             </header>
+            {
+              isEditing && (
+                <EditForm
+                  editedTask={editedTask}
+                  updateTask={updateTask}
+                />
+              )
+            }
             <CustomForm addTask={addTask}/>
             {tasks && (
               <TaskList
                 tasks={tasks}
                 deleteTask={deleteTask}
                 toggleTask={toggleTask}
+                enterEditMode={enterEditMode}
               />
             )}
             </div>
